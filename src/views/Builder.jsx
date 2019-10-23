bb1c9047e040e5df4839a897d387f3d1680ad59d
Gorka <gorka.molero@gmail.com>
2019-10-23 14:32:03 +0200

    Removing router, only one view for now

--- src/views/Builder.jsx
+++ src/views/Builder.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, useRouteMatch, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { Fab } from '@material-ui/core'
 import { Build, Palette } from '@material-ui/icons'
 import { Scrollbars } from 'react-custom-scrollbars'
@@ -8,10 +8,8 @@ import { TemplateProvider, initialState, TemplateReducer } from '../state/Templa
 
 import Preview from '../components/preview/Preview'
 import Content from '../components/content/Content'
-import Style from '../components/style/Style'
 
 function Builder() {
-  let match = useRouteMatch()
   return (
     <TemplateProvider initialState={initialState} reducer={TemplateReducer}>
       <div className="builder">
@@ -26,14 +24,7 @@ function Builder() {
         </nav>
         <Scrollbars autoHide style={{width: '100%', height: '100%'}}>
           <main className="controlContainer">
-            <Switch>
-              <Route path={`${match.path}/content`}>
-                <Content />
-              </Route>
-              <Route path={`${match.path}/style`}>
-                <Style />
-              </Route>
-            </Switch>
+            <Content />
           </main>
         </Scrollbars>
       </div>
