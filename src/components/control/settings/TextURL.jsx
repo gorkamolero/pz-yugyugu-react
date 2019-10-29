bbc9fa539a75aa572f36cbbed4b6578dda0495cd
Gorka <gorka.molero@gmail.com>
2019-10-29 15:46:03 +0100

    Updating labels

--- src/components/control/settings/TextURL.jsx
+++ src/components/control/settings/TextURL.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 import { TextField, InputAdornment } from '@material-ui/core'
 import { Public } from '@material-ui/icons'
 
+const FabTextField = styled(TextField)`
+  label {
+    background: white;
+    padding: 0 3px;
+  }
+`
+
+
+
 const TextOrURL = ({ value, title, onChange, type }) => {
   return (
-    <TextField
+    <FabTextField
       variant="outlined"
       label={title}
       value={value}
       multiline={value.length > 24 && !(value.includes('https'))}
       fullWidth
       margin="normal"
-      key={title}
       onChange={(e) => onChange(e.target.value)}
       InputProps={{
         startAdornment: value.includes('https') ? <InputAdornment position="start"><Public /></InputAdornment> : null,
